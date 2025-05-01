cac475ddd896bebeb617ee54afc3b29a42bc06d1
johnnyle24 <johnny.le24@gmail.com>
2025-04-30 17:38:32 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# XCancel Discord Bot
+
+Simple discord bot using python wrapper for discord library
+
+## QuickStart
+
+### Setup
+
+You must have pip3, python3, and the requisite libraries installed.
+
+```bash
+pip3 install discord
+python3 xcancel.py
+```
+
+### Troubleshooting
+
+If you see certificate errors, you will need to run
+
+```bash
+pip3 install certifi
+```
+
+If you receive 401 errors, the token is invalid
---
+++ xcancel.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        if message.content.startswith('https://x.com'):
+            xcancel_message = message.content.replace("https://x.com", "https://xcancel.com")
+            new_message = f"{xcancel_message} posted by {str(message.author.mention)}"
+            msg = await message.channel.send(new_message)
+            await asyncio.sleep(3.0)
+            await message.delete()
+
+
+intents = discord.Intents.default()
+intents.messages = True
+
+client = MyClient(intents=intents)
+client.run('TOKEN HERE')
