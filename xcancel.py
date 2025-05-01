84e848b5cd2c54edb14aa267f24079e7ae5a96a3
johnnyle24 <johnny.le24@gmail.com>
2025-05-01 09:38:08 -0700

    Use message_content due to library conflict

--- xcancel.py
+++ xcancel.py
@@ -16,7 +16,7 @@ class MyClient(discord.Client):
 
 
 intents = discord.Intents.default()
-intents.messages = True
+intents.message_content = True
 
 client = MyClient(intents=intents)
 client.run('TOKEN HERE')
